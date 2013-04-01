97bdbcabc5fa7f2c68dd49398fe85580c1419141
James Smith <james@floppy.org.uk>
2013-04-01 16:00:09 +0100

    Add confirmable flag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130401143544) do
+ActiveRecord::Schema.define(version: 20130401144954) do
 
   create_table "crops", force: true do |t|
     t.string   "name"
@@ -51,8 +51,12 @@ ActiveRecord::Schema.define(version: 20130401143544) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
