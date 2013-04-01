5fa71a2d3a6744edd6d7eee82a27876714d8f2cd
James Smith <james@floppy.org.uk>
2013-04-01 17:46:27 +0100

    Allow admins to view any pod

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -9,6 +9,9 @@ class PodsController < ApplicationController
       @pod = Pod.where(month: Date.today.beginning_of_month).first
     when 'preview'
       @pod = Pod.where(month: (Date.today - 3.months).beginning_of_month).first
+    else
+      raise ActiveRecord::RecordNotFound unless admin_signed_in?
+      @pod = Pod.where(month: Date.strptime(params[:id], "%Y-%m")).first
     end
     raise ActiveRecord::RecordNotFound if @pod.nil?
   end
