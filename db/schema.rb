71e1c5e22903cb177f3e6868387d1fbb60e09eb8
James Smith <james@floppy.org.uk>
2013-04-01 18:41:19 +0100

    Add ship flag to crops and include in shipment view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130401170150) do
+ActiveRecord::Schema.define(version: 20130401173532) do
 
   create_table "admins", force: true do |t|
     t.string   "email",               null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20130401170150) do
     t.integer  "pod_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "ship",       default: false
   end
 
   add_index "instructions", ["crop_id"], name: "index_instructions_on_crop_id"
