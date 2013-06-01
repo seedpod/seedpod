0dddf71b561f4024264557ee353e73974b9cd662
James Smith <james@floppy.org.uk>
2013-06-01 22:31:06 +0100

    update cookie store for rails 4 rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Seedpod::Application.config.session_store :encrypted_cookie_store, key: '_seedpod_session'
+Seedpod::Application.config.session_store :cookie_store, key: '_seedpod_session'
