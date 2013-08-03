ebc826953d3f12e6d93891f0a70a8b62d593a7a0
James Smith <james@floppy.org.uk>
2013-08-04 00:37:40 +0100

    Collect details on signup
    Resolves #5

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
+ 
+  before_filter :configure_permitted_parameters
+ 
   protected
-
+ 
+  # my custom fields are :name, :heard_how
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :address_street, :address_locality, :address_region, :address_postcode,
+        :email, :password, :password_confirmation, :current_password)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:name, :address_street, :address_locality, :address_region, :address_postcode,
+        :email, :password, :password_confirmation, :current_password)
+    end
+  end
+ 
+ 
   def after_sign_up_path_for(resource)
     new_user_subscription_path(resource)
   end
+  
 end
