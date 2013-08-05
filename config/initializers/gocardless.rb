c8a225357a13b8db8ef17aea856951cbb2c51f99
James Smith <james@floppy.org.uk>
2013-08-06 00:31:54 +0100

    Only initialize gocardless if the tokens are present

--- config/initializers/gocardless.rb
+++ config/initializers/gocardless.rb
@@ -1,6 +1,8 @@
-GoCardless.account_details = {
-  :app_id     => ENV['GOCARDLESS_APP_ID'],
-  :app_secret => ENV['GOCARDLESS_APP_SECRET'],
-  :token      => "#{ENV['GOCARDLESS_APP_TOKEN']}
-                  manage_merchant:#{ENV['GOCARDLESS_APP_MERCHANT']}",
-}
+if ENV['GOCARDLESS_APP_ID']
+  GoCardless.account_details = {
+    :app_id     => ENV['GOCARDLESS_APP_ID'],
+    :app_secret => ENV['GOCARDLESS_APP_SECRET'],
+    :token      => "#{ENV['GOCARDLESS_APP_TOKEN']}
+                    manage_merchant:#{ENV['GOCARDLESS_APP_MERCHANT']}",
+  }
+end
