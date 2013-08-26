0effde063ea9d22f7c880ee3471fc823e339e697
James Smith <james@floppy.org.uk>
2013-08-26 22:40:52 +0100

    Remove email form filling logic
    Resolves #28

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,3 @@
 class SessionsController < Devise::SessionsController
-  
-  def create
-    case params[:sign_in_action]
-    when 'sign_up'
-      redirect_to new_user_registration_path(:'email' => params[:user][:email])
-    else
-      super
-    end
-  end
-  
+
 end
