56dfa46e26eea514be5859836a93cd8f9588747b
James Smith <james@floppy.org.uk>
2013-08-26 22:20:55 +0100

    Users do not need to confirm email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :shipments
