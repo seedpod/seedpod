6a908e24bef67680f3ebd314938c5f235d358921
James Smith <james@floppy.org.uk>
2013-08-30 22:19:42 +0100

    Add a warning banner if there is no active subscription.
    Resolves #3

--- features/step_definitions/subscription_steps.rb
+++ features/step_definitions/subscription_steps.rb
@@ -5,6 +5,11 @@ end
 
 Given(/^I have a subscription set up$/) do
   @subscription = FactoryGirl.create :subscription, user: @user
+  @user.subscribed?.should be_true
+end
+
+Given(/^I do not have a subscription set up$/) do
+  @user.subscribed?.should be_false
 end
 
 Then(/^my subscription should be marked as cancelled$/) do
@@ -16,4 +21,12 @@ Then(/^my subscription should be cancelled$/) do
   sub = double("subscription")
   sub.should_receive(:cancel!).once
   Subscription.any_instance.should_receive(:gocardless_subscription).and_return(sub)
+end
+
+Then(/^I should see a subscription warning$/) do
+  page.should have_text("You don't have an active subscription right now")
+end
+
+Then(/^I should not see a subscription warning$/) do
+  page.should_not have_text("You don't have an active subscription right now")
 end
