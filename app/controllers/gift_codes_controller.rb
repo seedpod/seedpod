16b81674bc89f14a8b19f0dfcac58cfe52efad5f
James Smith <james@floppy.org.uk>
2013-11-20 08:21:25 +0000

    Add create action

--- app/controllers/gift_codes_controller.rb
+++ app/controllers/gift_codes_controller.rb
@@ -4,6 +4,15 @@ class GiftCodesController < ApplicationController
     @gift_code = GiftCode.new
   end
 
+  def create
+    @gift_code = GiftCode.create(gift_code_params)
+    if @gift_code.valid?
+      render :text => 'ok'
+    else
+      render :action => 'new'
+    end
+  end
+
   private
   
   def gift_code_price_map
@@ -16,4 +25,8 @@ class GiftCodesController < ApplicationController
   end
   helper_method :gift_code_price_map
   
+  def gift_code_params
+    params[:gift_code].permit(:months, :purchaser_name, :purchaser_email, :send_to_recipient, :recipient_email, :recipient_name, :send_date)
+  end
+
 end
