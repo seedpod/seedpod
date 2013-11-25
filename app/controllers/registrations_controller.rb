3360d36989fc03586c0e61adfd0c0d1c5ab0bf0e
James Smith <james@floppy.org.uk>
2013-11-25 21:33:39 +0000

    Pass gift code through to subscription controller instead of processing during user signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
   
   def after_sign_up_path_for(user)
-    if user.subscribed?
-      getting_started_pods_path      
-    else
-      new_user_subscription_path(user)
-    end
+    new_user_subscription_path(user, gift_code: (params[:user][:gift_code].blank? ? nil : params[:user][:gift_code]))
   end
   
 end
