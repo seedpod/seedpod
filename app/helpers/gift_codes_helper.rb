5392986ec4a06aec78d426a9467431c8085918de
James Smith <james@floppy.org.uk>
2013-12-01 15:25:54 +0000

    Add javascript preview of price for gift codes

--- app/helpers/gift_codes_helper.rb
+++ app/helpers/gift_codes_helper.rb
@@ -4,10 +4,17 @@ module GiftCodesHelper
     [3,6,9,12]
   end
 
-  def gift_code_options_for_select
+  def gift_code_length_options_for_select
     gift_code_lengths.map do |length|
-      ["#{length} Months - £%.2f" % Pod.price(months: length), length]
+      ["#{length} Months", length]
     end
+  end
+  
+  def gift_code_organic_options_for_select
+    [
+      [t(:non_organic), false],
+      [t(:organic), true]
+    ]
   end
 
   def discount_string(months)
