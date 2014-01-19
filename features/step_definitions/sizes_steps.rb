86409e646a78494a2cbbc28c715ebdb976d3ee15
James Smith <james@floppy.org.uk>
2014-01-19 14:19:47 +0000

    remove duplicate step

--- features/step_definitions/sizes_steps.rb
+++ features/step_definitions/sizes_steps.rb
@@ -1,8 +1,3 @@
-Given(/^the current pod has a crop "(.*?)" which is organic$/) do |name|
-  crop = FactoryGirl.create(:crop, name: name, organic: true, non_organic: false)
-  FactoryGirl.create(:instruction, crop: crop, pod: @current_pod, ship: true)
-end
-
 Given(/^the current pod has a crop "(.*?)" which is "(.*?)" "(.*?)"$/) do |name, size, is_organic|
   crop_options = {
     name: name
