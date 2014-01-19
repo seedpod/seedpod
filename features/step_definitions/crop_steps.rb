e5c702a23dd673a8895078377103b4bc7aeef47d
James Smith <james@floppy.org.uk>
2014-01-19 14:23:44 +0000

    update steps to set medium size on crops by default

--- features/step_definitions/crop_steps.rb
+++ features/step_definitions/crop_steps.rb
@@ -1,14 +1,14 @@
 Given(/^the current pod has a crop "(.*?)" which is organic$/) do |name|
-  crop = FactoryGirl.create(:crop, name: name, organic: true, non_organic: false)
+  crop = FactoryGirl.create(:crop, name: name, organic: true, non_organic: false, medium: true)
   FactoryGirl.create(:instruction, crop: crop, pod: @current_pod, ship: true)
 end
 
 Given(/^the current pod has a crop "(.*?)" which is non\-organic$/) do |name|
-  crop = FactoryGirl.create(:crop, name: name, organic: false, non_organic: true)
+  crop = FactoryGirl.create(:crop, name: name, organic: false, non_organic: true, medium: true)
   FactoryGirl.create(:instruction, crop: crop, pod: @current_pod, ship: true)
 end
 
 Given(/^the current pod has a crop "(.*?)" which is organic and non\-organic$/) do |name|
-  crop = FactoryGirl.create(:crop, name: name, organic: true, non_organic: true)
+  crop = FactoryGirl.create(:crop, name: name, organic: true, non_organic: true, medium: true)
   FactoryGirl.create(:instruction, crop: crop, pod: @current_pod, ship: true)
 end
